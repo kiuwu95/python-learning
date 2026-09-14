bae8dcf1df3a7d9f94979f00cf17e5e2d1038010
kiuwu95 <moziwa3301649843@gmail.com>
2026-09-14 13:30:29 +0800

    添加字符串定义示例，展示不同引号的用法及换行符和转义字符

---
+++ type/test.py
@@ -0,0 +1,8 @@
+﻿# type()用于查看数据的类型是什么，isinstance()用来查看数据的类型是否为某种指定的类型
+a = 1
+b = 3.14
+c = "hello,world"
+print(type(a), type(b), type(c))
+print(isinstance(c, str))
+print(isinstance(c, int))
+print(isinstance(c, float))
---
+++ 字符串定义/test.py
@@ -0,0 +1,22 @@
+﻿# 1.可用双引号
+str1 = "hello"
+# 2.可用单引号
+str2 = "world"
+# 3.可用三个引号（可加换行）
+str3 = """
+    你好
+        世界
+"""
+print(str1)
+print(str2)
+print(str3)
+
+# \n换行符，\t缩进
+str4 = "h\ne"
+str5 = "w\te"
+print(str4)
+print(str5)
+
+# \" \'用于在字符串里面添加双引号或单引号
+str6 = "114\"514'6666"
+print(str6)
